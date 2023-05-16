cf9978b6245001163765306daed85dd3629663b9
Shakeel Afridi <shakeel.afridi@cheetay.pk>
2023-05-16 23:07:55 +0500

    Fix: remove google secrets

--- client/constants.py
+++ client/constants.py
@@ -4,4 +4,4 @@ FITNESS_CENTER = 'Fitness Center'
 
 ALLOWED_TABS = ['info_tab', 'about_tab', 'portfolio_tab', 'ads_tab', 'contact_tab', 'gallery_tab']
 
-GOOGLE_API_KEY = 'AIzaSyCy7x2Sx_u2gBDfSBfksctKjknIHpAqy24'
+GOOGLE_API_KEY = ''
